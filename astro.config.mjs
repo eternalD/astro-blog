16b1233bb627a83bc71bbc0b8f69336521d1d0d5
Harri Kilpiö <harri.kilpio@gmail.com>
2024-01-08 22:15:01 +0200

    adds Vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	output: 'server',
+  	adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });

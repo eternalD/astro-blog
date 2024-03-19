85b84eac6480e57134a558106cabd5f169354e30
Harri Kilpiö <harri.kilpio@gmail.com>
2024-03-19 16:00:37 +0200

    initial prev /next navigation

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,12 +4,16 @@ const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+	title: z.string(),
+	description: z.string(),
+	// Transform string to Date object
+	pubDate: z.coerce.date(),
+	prevBlogSlug: z.string().optional(),
+	nextBlogSlug: z.string().optional(),
+	prevTitle: z.string().optional(),
+	nextTitle: z.string().optional(),
+	updatedDate: z.coerce.date().optional(),
+	heroImage: z.string().optional(),
 	}),
 });
 
@@ -17,13 +21,13 @@ const portfolio = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		tags: z.array(z.string().optional()),
+	title: z.string(),
+	description: z.string(),
+	// Transform string to Date object
+	pubDate: z.coerce.date(),
+	updatedDate: z.coerce.date().optional(),
+	heroImage: z.string().optional(),
+	tags: z.array(z.string().optional()),
 	}),
 });
 
